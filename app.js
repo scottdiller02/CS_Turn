b85d8c712210406de9705a0c7f4b351f1a183ed5
Scott <scott.diller@bobcats.gcsu.edu>
2017-04-25 11:23:53 -0400

    second version

---
+++ app.js
@@ -0,0 +1,96 @@
+var express=require("express");
+var http=require("http");
+var path=require("path");
+var handlebars=require("express-handlebars").create({defaultLayout:"home"});
+var bodyParser = require('body-parser');
+var db = require('./db');
+//var dbLink=require("./json/config.json");
+var dbLink=require("./json/dbproduction.json");
+var url = dbLink.devServer.url;
+var app=express();
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+//app.use(bodyParser.urlencoded({extended: true}));app.use(bodyParser.json());
+var publicPath=path.resolve(__dirname, "public"	);
+app.use(express.static(publicPath));
+//CHANGE app.use(require('./routers/popMenus'));
+//app.user routers
+//CHANGE app.use(require("./routers/getMenuItems"));
+//CHANGE app.use(require('./routers/signup'));
+//CHANGE app.use(require('./routers/processOrders'));
+// Connect to Mongo on start
+db.connect(url, function(err){
+    if (err) {
+    console.log('Unable to connect to Mongo.');
+    process.exit(1);
+} else {
+	var listener=http.createServer(app).listen(process.env.PORT||3000);
+	console.log('Server is listening at port'+listener.address().port);
+	//app.listen(3000, function() {
+    //  console.log('Listening on port 3000...')
+    //})
+ }
+})
+
+app.get("/",function(req,res){
+		console.log("Coming a request!");
+	res.sendFile(`${publicPath}/home.html`);
+});
+
+app.get("/home",function(req,res){
+		console.log("Coming amainrequest!");
+	res.sendFile(`${publicPath}/home.html`);
+});
+
+//app.get("/menu",function(req,res){
+//		console.log("Coming a menurequest!");
+//	res.sendFile(`${publicPath}/menu.html`);
+//});
+
+app.get("/welcome",function(req,res){
+		console.log("Coming a signuprequest!");
+	res.render(`welcome`);
+});
+
+app.get("/apartments",function(req,res){
+		console.log("Coming a request!");
+	res.sendFile(`${publicPath}/apartments.html`);
+});
+
+/*
+app.get("/cart",function(req,res){
+		console.log("Coming a signuprequest!");
+	res.render(`cart`);
+});
+*/
+/*app.get("/about",function(req,res){
+		console.log("Coming a request!");
+	res.sendFile(`${publicPath}/about.html`);
+});
+*/
+app.get("/signin",function(req,res){
+		console.log("Coming a signuprequest!");
+	res.render(`signin`);
+});
+
+//app.get("/getMenuItems", function(req, res){
+//
+
+//});
+
+//app.listen(3000, function () {
+//	console.log('Example app listening on port 3000!')
+//});
+
+/*app.post("/signupServer", function (req,res){
+		console.log("Coming a signup request!");
+	res.send("THe server is handling your request"+req.body.email);
+});
+*/
+
+//var listener=http.createServer(app).listen(process.env.PORT||3000);
+//console.log('Server is listening at port'+listener.address().port);
+
+app.set('db',db);
+module.exports.app=app;
